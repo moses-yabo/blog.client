f8b11df8054be7a41117c06d88b8f71c0f304183
moses-yabo <yabo.lindile@younglings.africa>
2024-12-13 16:08:38 +0200

    added_signup

--- src/pages/Signin/Signin.js
+++ src/pages/Signin/Signin.js
@@ -1,16 +1,54 @@
 import React, { useState, useEffect } from "react";
-
+import { gql ,useMutation} from "@apollo/client";
 import { Form } from "react-bootstrap";
 // import Button from "@restart/ui/esm/Button";
 
+// {
+//   email:"moses@spazio.co.za",password:"0780431060"
+// }
+
+const SIGNIN = gql`
+mutation Signin($email:String!,$password:String!){
+  signin(credentials:{
+    email:$email,
+    password:$password
+  }){
+    userErrors {
+      message
+    }
+    token
+  }
+}
+`
+
 export default function Signin() {
+  const [signin,{data,loading}] = useMutation(SIGNIN);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  console.log(data);
+  
 
-  const handleClick = () => {};
+  const handleClick = (e) => {
+    e.preventDefault()
+    signin({
+      variables:{
+        email,
+        password
+      }
+    })
+  };
 
   const [error, setError] = useState(null);
-
+  useEffect(() =>{
+    if (data) {
+      if (data.signin.userErrors.length) {
+        setError(data.signin.userErrors[0].message)
+      }
+      if (data.signin.token) {
+        localStorage.setItem("token",data.signin.token)
+      }
+    }
+  },[data])
   return (
     <div>
       <Form>
